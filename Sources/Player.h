f73e069c685a82e59458c9d0cf7b2b5a76feda3a
Danielius Sauseris <114989641+Danisss7@users.noreply.github.com>
2022-12-12 00:42:28 +0200

    Update Player.h

--- Sources/Player.h
+++ Sources/Player.h
@@ -14,16 +14,25 @@ private:
 
 	float movementSpeed;
 
+	float attackCooldown;
+	float attackCooldownMax;
+
 	//Private functions
+	void initVariables();
 	void initTexture();
 	void initSprite();
 public:
 	Player();
 	virtual ~Player();
 
+	//Accessor
+	const sf::Vector2f& getPos() const;
+
 	//Functions
 	void move(const float dirX, const float dirY);
+	const bool canAttack();
 
+	void updateAttack();
 	void update();
 	void render(sf::RenderTarget& target);
 };
