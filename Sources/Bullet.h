cceb7301e9e049a0921cbfa46920875129d911ca
Danielius Sauseris <114989641+Danisss7@users.noreply.github.com>
2022-12-12 00:41:57 +0200

    Add files via upload

---
+++ Sources/Bullet.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Bullet
+{
+private:
+
+	sf::Sprite shape;
+
+	sf::Vector2f direction;
+	float movementSpeed;
+
+public:
+	Bullet();
+	Bullet(sf::Texture * texture, float pos_x, float pos_y, float dir_X, float dir_Y, float movement_speed);
+	virtual ~Bullet();
+
+	//Accessor
+	const sf::FloatRect getBounds() const;
+
+	void update();
+	void render(sf::RenderTarget* target);
+};
+
