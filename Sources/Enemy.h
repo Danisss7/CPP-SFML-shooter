6751b1ff9282ee7be6490d8fab815fa3a1d4300d
Danielius Sauseris <114989641+Danisss7@users.noreply.github.com>
2022-12-12 06:12:06 +0200

    Add files via upload

---
+++ Sources/Enemy.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Enemy
+{
+private:
+	sf::CircleShape shape;
+	int type;
+	float speed;
+	int hp;
+	int hpMax;
+	int damage;
+	int points;
+
+	void initShape();
+	void initVariables();
+
+public:
+	Enemy(float pos_x, float pos_y);
+	virtual ~Enemy();
+
+	//Accessors
+	const sf::FloatRect getBounds() const;
+
+	//Functions
+	void update();
+	void render(sf::RenderTarget* target);
+};
