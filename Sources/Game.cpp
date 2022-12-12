601a77af23feade49b3573a54455d4cfadc30dce
Danielius Sauseris <114989641+Danisss7@users.noreply.github.com>
2022-12-12 06:10:55 +0200

    Update Game.cpp

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -14,9 +14,40 @@ void Game::initTextures()
 	this->textures["BULLET"]->loadFromFile("Resources/bullet.png");
 }
 
+void Game::initGUI()
+{
+	//Load fonts
+	if (!this->font.loadFromFile("Fonts/PLANK.TTF"))
+		std::cout << "ERROR::GAME::Failed to load font" << "/n";
+
+	//Initialize point text
+	this->pointText.setFont(this->font);
+	this->pointText.setCharacterSize(12);
+	this->pointText.setFillColor(sf::Color::White);
+	this->pointText.setString("Test");
+}
+
+void Game::initWorld()
+{
+	if (!this->worldBackgroundTex.loadFromFile("Resources/background.jpg"))
+		std::cout << "ERROR::GAME:: COULD NOT LOADD BACKGROUND TEXTURE" << "/n";
+	this->worldBackground.setTexture(this->worldBackgroundTex);
+}
+
+void Game::initSystems()
+{
+	this->points = 0;
+}
+
 void Game::initPlayer()
 {
 	this->player = new Player();
+}
+
+void Game::initEnemies()
+{
+	this->spawnTimerMax = 50.f;
+	this->spawnTimer = this->spawnTimerMax;
 }
 
 //Constructor - Destructor
@@ -24,7 +55,11 @@ Game::Game()
 {
 	this->initWindow();
 	this->initTextures();
+	this->initGUI();
+	this->initWorld();
+	this->initSystems();
 	this->initPlayer();
+	this->initEnemies();
 }
 
 Game::~Game()
@@ -40,6 +75,12 @@ Game::~Game()
 
 	//Delete bullets
 	for (auto* i : this->bullets)
+	{
+		delete i;
+	}
+
+	//Delete enemies
+	for (auto* i : this->enemies)
 	{
 		delete i;
 	}
@@ -81,8 +122,46 @@ void Game::updateInput()
 
 	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && this->player->canAttack())
 	{
-		this->bullets.push_back(new Bullet(this->textures["BULLET"], this->player->getPos().x, this->player->getPos().y, 0.f, -1.f, 5.f));
+		this->bullets.push_back(new Bullet(this->textures["BULLET"], this->player->getPos().x + this->player->getBounds().width/2.f, this->player->getPos().y, 0.f, -1.f, 5.f));
 	} 
+}
+
+void Game::updateGUI()
+{
+	std::stringstream ss;
+
+	ss << "Points: " << this->points;
+
+	this->pointText.setString(ss.str());
+}
+
+void Game::updateWorld()
+{
+
+}
+
+void Game::updateCollision()
+{
+	//Left world collision
+	if (this->player->getBounds().left < 0.f)
+	{
+		this->player->setPosition(0.f, this->player->getBounds().top);
+	}
+	//Right world collision
+	else if (this->player->getBounds().left +this->player->getBounds().width >= this->window->getSize().x)
+	{
+		this->player->setPosition(this->window->getSize().x - this->player->getBounds().width, this->player->getBounds().top);
+	}
+	//Top world collision
+	if (this->player->getBounds().top < 0.f)
+	{
+		this->player->setPosition(this->player->getBounds().left, 0.f);
+	}
+	//Bottom world collision
+	else if (this->player->getBounds().top + this->player->getBounds().height > this->window->getSize().y)
+	{
+		this->player->setPosition(this->player->getBounds().left, this->window->getSize().y - this->player->getBounds().height);
+	}
 }
 
 void Game::updateBullets()
@@ -105,6 +184,55 @@ void Game::updateBullets()
 	}
 }
 
+void Game::updateEnemies()
+{
+	//Spawning
+	this->spawnTimer += 0.5f;
+	if (this->spawnTimer >= this->spawnTimerMax)
+	{
+		this->enemies.push_back(new Enemy(rand() % this->window->getSize().x - 20.f, -100.f));
+		this->spawnTimer = 0.f;
+	}
+
+	//Update
+	unsigned counter = 0;
+	for (auto* enemy : this->enemies)
+	{
+		enemy->update();
+
+		//removing bullet when its hitting the top of the window
+		if (enemy->getBounds().top > this->window->getSize().y)
+		{
+			//Delete bullets
+			delete this->enemies.at(counter);
+			this->enemies.erase(this->enemies.begin() + counter);
+			--counter;
+		}
+		++counter;
+	}
+}
+
+void Game::updateCombat()
+{
+	for (int i = 0; i < this->enemies.size(); ++i)
+	{
+		bool enemy_deleted = false;
+		for (size_t k = 0; k < this->bullets.size() && enemy_deleted == false; k++)
+		{
+			if (this->enemies[i]->getBounds().intersects(this->bullets[k]->getBounds()))
+			{
+				delete this->enemies[i];
+				this->enemies.erase(this->enemies.begin() + i);
+
+				delete this->bullets[k];
+				this->bullets.erase(this->bullets.begin() + k);
+
+				enemy_deleted = true;
+			}
+		}
+	}
+}
+
 void Game::update()
 {
 	this->updatePollEvents();
@@ -113,12 +241,35 @@ void Game::update()
 
 	this->player->update();
 
+	this->updateCollision();
+
 	this->updateBullets();
+
+	this->updateEnemies();
+
+	this->updateCombat();
+
+	this->updateGUI();
+
+	this->updateWorld();
+}
+
+void Game::renderGUI()
+{
+	this->window->draw(this->pointText);
+}
+
+void Game::renderWorld()
+{
+	this->window->draw(this->worldBackground);
 }
 
 void Game::render()
 {
 	this->window->clear();
+
+	//Draw world
+	this->renderWorld();
 
 	//Draw everything
 	this->player->render(*this->window);
@@ -127,6 +278,13 @@ void Game::render()
 	{
 		bullet->render(this->window);
 	}
+
+	for (auto* enemy : this->enemies)
+	{
+		enemy->render(this->window);
+	}
+
+	this->renderGUI();
 
 	this->window->display();
 }
